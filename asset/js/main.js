0b3e1786199ca9762c8df561a50e1db42a11d553
andrea1632 <andrea.bellucci10@libero.it>
2022-03-11 15:56:51 +0100

    FizzBuzz complete

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,19 +3,15 @@ console.log(container);
 //ciclo for per determinare se il numero è multiplo di 3/5/3 && 5;
 for (var i = 1; i <= 100; i++) {
     if ( i % 3 == 0 && i % 5 == 0 ) {
-      console.log("FizzBuzz");
-      container.innerHTML += `<div class="p-3 bg-danger col">FizzBuzz</div>`
+      container.innerHTML += `<div class="p-3 m-2 bg-danger col-2 sizingCols d-flex align-items-center justify-content-center fw-bold">FizzBuzz</div>`
     }
     else if ( i % 3 == 0 ) {
-      console.log("Fizz");
-      container.innerHTML += `<div class="p-3 bg-success col">Fizz</div>`
+      container.innerHTML += `<div class="p-3 m-2 bg-success col-2 sizingCols d-flex align-items-center justify-content-center fw-bold">Fizz</div>`
     }
     else if ( i % 5 == 0 ) {
-      console.log("Buzz");
-      container.innerHTML += `<div class="p-3 bg-warning col">Buzz</div>`
+      container.innerHTML += `<div class="p-3 m-2 bg-warning col-2 sizingCols d-flex align-items-center justify-content-center fw-bold">Buzz</div>`
     }
     else {
-      console.log(i);
-      container.innerHTML += `<div class="p-3 bg-primary col">${i}</div>`
+      container.innerHTML += `<div class="p-3 m-2 bg-primary col-2 sizingCols d-flex align-items-center justify-content-center fs-2 fw-bold">${i}</div>`
     }
    }
