17f2513a3d793ec6b713c8a09c7d9b3740d074c9
andrea1632 <andrea.bellucci10@libero.it>
2022-03-11 15:39:32 +0100

    fizzbuzz stampato in html

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,15 +1,21 @@
+let container = document.getElementById("rowId");
+console.log(container);
 //ciclo for per determinare se il numero è multiplo di 3/5/3 && 5;
 for (var i = 1; i <= 100; i++) {
     if ( i % 3 == 0 && i % 5 == 0 ) {
       console.log("FizzBuzz");
+      container.innerHTML += `<div class="p-3 bg-danger col">FizzBuzz</div>`
     }
     else if ( i % 3 == 0 ) {
       console.log("Fizz");
+      container.innerHTML += `<div class="p-3 bg-success col">Fizz</div>`
     }
     else if ( i % 5 == 0 ) {
       console.log("Buzz");
+      container.innerHTML += `<div class="p-3 bg-warning col">Buzz</div>`
     }
     else {
       console.log(i);
+      container.innerHTML += `<div class="p-3 bg-primary col">${i}</div>`
     }
    }
